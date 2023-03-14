ad0acf2fcff1f9e1f1e25e75709f7861978b66b0
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2023-03-15 00:05:21 +0530

    Goerli testnet added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
 };
